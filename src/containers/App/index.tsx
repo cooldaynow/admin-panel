a178ccbf4b2cc8054a0bf26b25abf5bfed9d80b3
Alexandr Cheblukov <aacheblukov@lad24.ru>
2020-01-11 12:16:02 +0300

    remove package-lock.json

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -1,23 +1,51 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
 import { Button } from 'antd';
-import { fetchUsers } from 'store/actions/user';
+import { connect } from 'react-redux';
+import { Action, Dispatch } from 'redux';
+import TState from '../../store/i';
+import { fetchUsers } from '../../store/actions/user';
+import { TUserCollection } from '../../entities/user';
 import styles from './index.css';
 
-const App = () => {
-  const dispatch = useDispatch();
-  const getUsers = () => dispatch(fetchUsers.request());
-  return (
-    <div className={styles.wrapper}>
-      <div className={styles.block}>
-        <h1 className={styles.transition}>Hello World!</h1>
-        <span>This is test redux-saga dispatch :)</span>
-        <Button size='large' type='primary' onClick={getUsers}>
-          Fetch users !
-        </Button>
-      </div>
-    </div>
-  );
+type TAppProps = {
+  users: TUserCollection;
+  getUsers: () => Action;
 };
 
-export default App;
+class App extends React.Component<TAppProps> {
+  state = {
+    info: 'users'
+  };
+
+  componentDidMount(): void {
+    const { getUsers } = this.props;
+    getUsers();
+  }
+  showUsers = (users: TUserCollection) => (): void => {
+    const { info } = this.state;
+    console.log(users, info);
+  };
+
+  render() {
+    const { users } = this.props;
+    return (
+      <div className={styles.wrapper}>
+        <div className={styles.block}>
+          <h1 className={styles.transition}>Hello World!</h1>
+          <span>This is test redux-saga dispatch :)</span>
+          <Button size='large' type='primary' onClick={this.showUsers(users)}>
+            Fetch users !
+          </Button>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state: TState) => ({
+  users: state.usersReducer.data
+});
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  getUsers: () => dispatch(fetchUsers.request())
+});
+export default connect(mapStateToProps, mapDispatchToProps)(App);
